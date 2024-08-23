40925a5e8c18a56e4440900690f05f424777a7e9
Bora Özkoç <boraozkoc@hotmail.com>
2024-08-23 21:59:15 +0300

    LocationPreviewView Sheet completed

---
+++ SwiftUiMapApp/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  SwiftUiMapApp
+//
+//  Created by Bora ÖZKOÇ on 23.08.2024.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
